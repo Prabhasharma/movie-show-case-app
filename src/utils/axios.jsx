db5e9011669cdd93a6cb94733a041d084f155aa7
Prabhasharma <ps7360542@gmail.com>
2025-03-27 15:29:51 +0530

    changes commit

---
+++ src/utils/axios.jsx
@@ -0,0 +1,14 @@
+import axios from "axios";
+
+const instance = axios.create({
+    baseURL: "https://api.themoviedb.org/3/",
+    headers: {
+        Accept: "application/json",
+        Authorization: import.meta.env.VITE_REACT_APP_TOKEN,
+    },
+    params: {
+        api_key: import.meta.env.VITE_REACT_APP_API_KEY, 
+    }
+});
+
+export default instance;
